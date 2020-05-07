94d49f73cb8a38671f069c70cf1f03e27f4507be
maria larchenko <mashalarchenko16@gmail.com>
2020-05-07 16:34:41 +0300

    refactor: delete unused code

--- src/controller/eventHandlers.js
+++ src/controller/eventHandlers.js
@@ -11,7 +11,6 @@ const listenEvents = () => {
   const startGameBtn = document.querySelector('.start_btn');
   const stopGameBtn = document.querySelector('.stop_btn');
   const resetGameBtn = document.querySelector('.reset_btn');
-  // const puzzles = document.querySelectorAll('.puzzle_item');
   const puzzlesContainer = document.querySelector('.puzzle_container');
 
 
@@ -21,24 +20,11 @@ const listenEvents = () => {
       startGame();
     }
     state.isStarted = true;
-    console.log(111111, puzzlesContainer);
-
     puzzlesContainer.addEventListener('click', (event) => {
       if (event.target.classList.contains('puzzle_item')) {
-        console.log('clicked');
         clickPuzzle(event.target);
       }
     });
-    // puzzles.forEach((puzzle) => {
-    //   console.log(puzzle);
-    //   puzzle.addEventListener('click', () => {
-    //     console.log('clicked');
-    //   });
-    // return puzzle.addEventListener('click', (event) => {
-    //   console.log(event);
-    //   clickPuzzle(event.target);
-    // });
-    // });
   };
 
   if (isStartGame) {
@@ -53,15 +39,6 @@ const listenEvents = () => {
       resetGame();
     });
   }
-
-  // if (state.isStarted) {
-  //   console.log(111111);
-  //   pussles.forEach((puzzle) => {
-  //     puzzle.addEventListener('click', (event) => {
-  //       clickPuzzle(event.target);
-  //     });
-  //   });
-  // }
 };
 
 const listenStartApp = () => {
